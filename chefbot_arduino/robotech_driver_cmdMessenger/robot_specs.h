0cc2f767ea8d59406e161355725e252bc7d91392
romrobotics <server01.psa1981@gmail.com>
2021-12-16 19:29:37 +0630

    hector odom

---
+++ chefbot_arduino/robotech_driver_cmdMessenger/robot_specs.h
@@ -0,0 +1,18 @@
+#ifndef ROBOT_SPECS_H
+#define ROBOT_SPECS_H
+
+#define encoder_pulse   11
+#define gear_ratio      30
+#define wheel_diameter  0.090   // meter
+#define track_width     0.238   // meter ////edit
+#define MAX_RPM         320 /// edit
+#define pi              3.1415926
+#define two_pi          6.2831853
+#define new_enc_ticks   1320
+#define RKp             0.6
+#define RKi             0.00001
+#define RKd             0.4
+#define LKp             1.0
+#define LKi             0.00001
+#define LKd             0.0001
+#endif
